ca5eb980112f2e799ae8042340d44f53bb2c6858
teuxsd16 <31145959+teuxsd16@users.noreply.github.com>
2021-06-01 12:14:36 -0300

    Delete jacobi2.m

--- jacobi2.m
+++
@@ -1,1 +0,0 @@
-x0=zeros(11,1);qtd=0;iter=1;while qtd<3 && iter<200   x(1)=x0(2);   for j=2:10    x(j)=(cos(j/10)+x0(j-1)+x0(j+1))/5;   end   x(11)=x0(10)/2;   erro=norm(x-x0,'inf');    if erro<10^-5      qtd=qtd+1;   else   qtd=0;   end   x0=x;   iter=iter+1;   enddisp('Jacobi:')disp(x);    
